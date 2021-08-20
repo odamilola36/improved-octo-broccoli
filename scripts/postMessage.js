f457f33463f6fd07e79a2fef40255eb319bde678
odamilola36 <Odamilola36@gmail.com>
2021-08-20 17:48:29 +0100

    first commit

---
+++ scripts/postMessage.js
@@ -0,0 +1,42 @@
+let title = document.querySelector(".title")
+let nameC = document.querySelector(".name")
+let email = document.querySelector(".email")
+let message = document.querySelector(".message")
+let submit = document.querySelector(".submit")
+
+
+const requestBody = {
+    title: title.innerHTML.trim(),
+    name: nameC.innerHTML.trim(),
+    sender: email.innerHTML.trim(),
+    message: message.innerHTML.trim()
+}
+
+
+const options = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+};
+
+
+submit.addEventListener("click", function(e){
+    e.preventDefault();
+    title.innerHTML=""
+    nameC.innerHTML=""
+    email.innerHTML=""
+    message.innerHTML=""
+    fetch('https://goresume-api.herokuapp.com/messages', options)
+    .then(data => {
+        console.log(data);
+        if (!data.status) {
+            throw Error(data.status);
+        }
+        console.log(data)
+        return data.json();
+        }).then(update => {
+        console.log(update);
+    })
+})
